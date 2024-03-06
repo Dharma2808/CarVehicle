8404752a10ac4533d5bbc729d759e370dd3d911e
Venkata Dharma Teja Konakanchi <vk35@usf.edu>
2024-03-06 16:16:53 -0500

    Bike class added

---
+++ Bike.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal class Bike : Vehicle, RoadRules
+    {
+        public string horn()
+        {
+            return("Bikers, don't horn unnecessarily");
+        }
+
+        public string speedDown()
+        {
+            return("Bikers, reduce your speed");
+        }
+
+        public string speedUp()
+        {
+            return("Bikers, you can speed up");
+        }
+
+        public string Color;
+        public int Gears;
+        
+        public Bike()
+        {
+            this.model = "R15";
+            this.make = 2020;
+            Color = "Red";
+            Gears = 5;
+        }
+
+        public Bike(string model, string color, int make, int gears) 
+        {
+            this.model = model;
+            this.make = make;
+            Color = color;
+            Gears=gears;
+        }
+
+        public string getInfo()
+        {
+            return "The model of the bike is" + getModel();
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
             Vehicle v1= new Vehicle();
             Console.WriteLine(v1.getModel());
+
+            Bike B1 = new Bike();
+            Console.WriteLine(B1.ge
         }
     }
 }
